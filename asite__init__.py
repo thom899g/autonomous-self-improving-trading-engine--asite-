40cba3821b45e8f5d8a8af9b24edd12a965304ca
thom899g <153515950+thom899g@users.noreply.github.com>
2026-02-25 21:42:30 +0100

    Automated commit by HiveMind

---
+++ asite__init__.py
@@ -0,0 +1,8 @@
+"""
+Autonomous Self-Improving Trading Engine (ASITE)
+Core package for autonomous trading system
+"""
+
+__version__ = "1.0.0"
+__author__ = "Evolution Ecosystem"
+__description__ = "Autonomous trading system with self-improving capabilities"
